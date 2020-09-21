14d48efdb29d32ef495a4c1217acc0d2045556ff
MirCarapyx <69626816+MirCarapyx@users.noreply.github.com>
2020-09-21 17:06:23 +0800

    Create hh.sql

---
+++ hh.sql
@@ -0,0 +1,18 @@
+SELECT id AS ID,pid AS 父ID ,levels AS 父到子之间级数, paths AS 父到子路径 FROM (
+     SELECT id,pid,
+     @le:= IF (pid = 0 ,0,  
+         IF( LOCATE( CONCAT('|',pid,':'),@pathlevel)   > 0  ,      
+                  SUBSTRING_INDEX( SUBSTRING_INDEX(@pathlevel,CONCAT('|',pid,':'),-1),'|',1) +1
+        ,@le+1) ) levels
+     , @pathlevel:= CONCAT(@pathlevel,'|',id,':', @le ,'|') pathlevel
+      , @pathnodes:= IF( pid =0,',0', 
+           CONCAT_WS(',',
+           IF( LOCATE( CONCAT('|',pid,':'),@pathall) > 0  , 
+               SUBSTRING_INDEX( SUBSTRING_INDEX(@pathall,CONCAT('|',pid,':'),-1),'|',1)
+              ,@pathnodes ) ,pid  ) )paths
+    ,@pathall:=CONCAT(@pathall,'|',id,':', @pathnodes ,'|') pathall 
+        FROM  treenodes, 
+    (SELECT @le:=0,@pathlevel:='', @pathall:='',@pathnodes:='') vv
+    ORDER BY  pid,id
+    ) src
+ORDER BY id
